9b21867b1f924ae789a2e243877bdc3f31631602
ByronEG635 <105372022+Efrain635@users.noreply.github.com>
2025-03-13 14:28:45 -0600

    Actualizacion1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./database/authcontext";
-import ProtectedRoute from "./components/ProtectedRoute"; 
-import Login from './views/Login'
-import Encabezado from "./components/Encabezado";
+import ProtectedRoute from "./Components/ProtectedRoute"; 
+import Login from './Views/Login'
+import Encabezado from "./Components/Encabezado";
 import Inicio from "./Views/Inicio";
 
 import './App.css'
